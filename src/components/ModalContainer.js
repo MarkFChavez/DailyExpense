d2f0a58f3c5d313796a27ce81716a40b3017bec1
Mark Chavez <markchav3z@gmail.com>
2017-09-23 17:52:12 +0800

    keyboard avoiding view

--- src/components/ModalContainer.js
+++ src/components/ModalContainer.js
@@ -1,24 +1,86 @@
 import React from 'react'
-import { View, Text, Modal, TouchableHighlight } from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  Modal,
+  TouchableOpacity,
+  StatusBar,
+  KeyboardAvoidingView
+} from 'react-native'
 import { connect } from 'react-redux'
 
 const ModalContainer = props => {
   return (
-    <View style={{ marginTop: 22 }}>
-      <Modal
-        animationType="slide"
-        transparent={false}
-        visible={props.modalVisible}
-        onRequestClose={() => {
-          alert('Modal has been closed.')
-        }}
-      >
-        <Text>hello</Text>
-      </Modal>
-    </View>
+    <Modal
+      animationType="slide"
+      transparent={false}
+      visible={props.modalVisible}
+    >
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.modalContainer}>
+        <KeyboardAvoidingView behavior="padding" style={styles.formContainer}>
+          <View style={styles.innerContainer}>
+            <TextInput
+              autoCorrect={false}
+              style={styles.textfield}
+              placeholder={'Where did that money go?'}
+            />
+          </View>
+
+          <View style={styles.innerContainer}>
+            <TextInput
+              autoCorrect={false}
+              keyboardType={'numeric'}
+              style={styles.textfield}
+              placeholder={'How much I spent on it?'}
+            />
+          </View>
+
+          <View style={styles.innerContainer}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Add Expense</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+      </View>
+    </Modal>
   )
 }
 
+const styles = {
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch'
+  },
+  formContainer: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  innerContainer: {
+    marginVertical: 20
+  },
+  textfield: {
+    height: 40,
+    marginHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#000'
+  },
+  button: {
+    width: 150,
+    borderWidth: 1,
+    borderColor: '#000',
+    alignSelf: 'center',
+    paddingVertical: 10
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '300',
+    fontFamily: 'Avenir Next'
+  }
+}
 const mapStateToProps = ({ modalVisible }) => {
   return { modalVisible }
 }
